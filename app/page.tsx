2044eda5f068d1dbff80062f9821fd5f00d13865
Arsalan Ansari <arsalan.ansari@samarth.ac.in>
2025-06-04 16:27:50 +0530

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+
 export default function Page() {
   return (
     <main className="row-start-2 grid place-items-center">
-      <h1 className="text-xl font-bold">Hello, World!</h1>
+      <section className="space-y-4 text-center">
+        <h1 className="text-3xl font-bold">Next-Auth</h1>
+        <Button asChild>
+          <Link href="/login">Login</Link>
+        </Button>
+      </section>
     </main>
   );
 }
